b67af91a30c44302e915e9b5721c0478ce0e6da4
Pavel Strakhov <ri@idzaaus.org>
2018-07-07 16:55:33 +0300

    timers added

--- game.lua
+++ game.lua
@@ -1,19 +1,10 @@
-local game = {}
-
-game.field = {}
-
-local time_since_last_add = 0
 local starting_adds_left = 0
 local starting_adds_interval = 0.1
 local adds_interval = 5
-
-local time_since_floating_figure_update = 0
 local floating_figure_update_interval = 0.05
-
-local time_since_floating_check_request = nil
 local floating_check_interval = 0.5
 
-local figures = {}
+local game = {}
 
 local Value = {
   Free = 0,
@@ -23,17 +14,7 @@ local Value = {
 }
 game.Value = Value
 
-figures[1] = {
-  { { 1 } } -- single block
-}
-figures[2] = {
-  { { 1, 1 } } -- domino
-}
-figures[3] = {
-  { { 1, 1, 1 } }, -- I
-  { { 1, 1 }, { 1, 0 } } -- L
-}
-figures[4] = {
+local figures = {
   { { 1, 1, 1, 1 } }, -- I
   { { 1, 1 }, { 1, 1 } }, -- O
   { { 1, 1, 1 }, { 0, 1, 0 } }, -- T
@@ -43,80 +24,46 @@ figures[4] = {
   { { 1, 1, 0 }, { 0, 1, 1 } } -- Z
 }
 
-game.current_figure = nil
+local update_handlers = {}
 
-function game.start()
-  local field_size = { width = 10, height = 20 }
-  for y = 1, field_size.height do
-    game.field[y] = {}
-    for x = 1, field_size.width do
-      game.field[y][x] = Value.Free
+local function table_find(table1, val)
+  for i, v in ipairs(table1) do
+    if v == val then
+      return i
     end
   end
-  game.current_figure = nil
-  starting_adds_left = math.floor(field_size.height / 3)
+  return false
 end
 
-local x_selection = 1
-
-local function first_enabled_y(grid, x)
-  for y = 1, #grid do
-    if grid[y][x] == Value.Solid then
-      return y
-    end
-  end
-  return nil
-end
-
-local function calc_selection_y()
-  local final_y = nil
-  for x = 1, #game.current_figure[1] do
-    local y1 = first_enabled_y(game.current_figure, x)
-    local y2 = first_enabled_y(game.field, x + x_selection - 1)
-    if y1 and y2 then
-      local y = y2 - y1 + 1
-      if final_y then
-        final_y = math.max(final_y, y)
-      else
-        final_y = y
+local function Timer(interval, callback)
+  local obj = { interval = interval, callback = callback, elapsed = 0 }
+  obj.update = function(dt)
+    obj.elapsed = obj.elapsed + dt
+      while obj.elapsed > obj.interval do
+        obj.elapsed = obj.elapsed - obj.interval
+        obj.callback()
       end
+  end
+  obj.start = function()
+    if not table_find(update_handlers, obj.update) then
+      table.insert(update_handlers, obj.update)
     end
   end
-  if final_y and final_y < #game.field - #game.current_figure + 1 then
-    return final_y
-  else
-    return #game.field - #game.current_figure + 1
+  obj.restart = function()
+    obj.elapsed = 0
+    obj.start()
   end
+  obj.stop = function()
+    local i = table_find(update_handlers, obj.update)
+    if i then table.remove(update_handlers, i) end
+  end
+  return obj
 end
 
-local function add_to_field()
-  for x = 1, #game.field[1] do
-    if game.field[1][x] == Value.Solid then
-      -- game over
-      game.start()
-      return
-    end
-  end
-  table.remove(game.field, 1)
 
-  local new_line = {}
-  for x = 1, #game.field[1] do
-    if math.random() > 0.2 then
-      new_line[x] = Value.Solid
-    else
-      new_line[x] = Value.Free
-    end
-  end
-  table.insert(game.field, new_line)
-  time_since_floating_check_request = 0
-end
-
-local function get_field_item(x, y)
-  if x < 1 or x > #game.field[1] or y < 1 or y > #game.field then
-    return -1
-  end
-  return game.field[y][x]
-end
+game.field = {}
+game.current_figure = nil
+local x_selection = 1
 
 local function fix_x_selection_bounds()
   if x_selection < 1 then
@@ -148,7 +95,16 @@ local function is_removable_figure()
   return false
 end
 
-local function mark_orphans()
+local function get_field_item(x, y)
+  if x < 1 or x > #game.field[1] or y < 1 or y > #game.field then
+    return -1
+  end
+  return game.field[y][x]
+end
+
+local orphan_check_timer
+orphan_check_timer = Timer(floating_check_interval, function()
+  orphan_check_timer.stop()
   for x = 1, #game.field[1] do
     if game.field[#game.field][x] == Value.Solid then
       game.field[#game.field][x] = Value.SolidMarked
@@ -182,78 +138,135 @@ local function mark_orphans()
       end
     end
   end
-  if not is_removable_figure() then
+  if game.current_figure and not is_removable_figure() then
     game.generate_figure()
   end
-end
+end)
 
-function love.update(dt)
-  time_since_last_add = time_since_last_add + dt
-  if starting_adds_left > 0 then
-    if time_since_last_add > starting_adds_interval then
-      add_to_field()
-      time_since_last_add = time_since_last_add - starting_adds_interval
-      starting_adds_left = starting_adds_left - 1
-      if starting_adds_left == 0 then
-        game.generate_figure()
-      end
-    end
-  else
-    if time_since_last_add > adds_interval then
-      add_to_field()
-      time_since_last_add = time_since_last_add - adds_interval
+local function add_to_field()
+  for x = 1, #game.field[1] do
+    if game.field[1][x] == Value.Solid then
+      -- game over
+      game.start()
+      return
     end
   end
+  table.remove(game.field, 1)
 
-  time_since_floating_figure_update = time_since_floating_figure_update + dt
-  if time_since_floating_figure_update > floating_figure_update_interval then
-    time_since_floating_figure_update = time_since_floating_figure_update - floating_figure_update_interval
-    for x = 1, #game.field[1] do
-      if game.field[1][x] == Value.Floating then
-        -- float outside of the field
-        game.field[1][x] = Value.Free
-      end
+  local new_line = {}
+  for x = 1, #game.field[1] do
+    if math.random() > 0.2 then
+      new_line[x] = Value.Solid
+    else
+      new_line[x] = Value.Free
     end
-    for y = 2, #game.field do
-      for x = 1, #game.field[y] do
-        if game.field[y][x] == Value.Floating then
-          if game.field[y - 1][x] == Value.Free then
-            -- keep floating up
-            game.field[y - 1][x] = Value.Floating
-            game.field[y][x] = Value.Free
-          else
-            -- can't float! become solid
-            game.field[y][x] = Value.Solid
-          end
+  end
+  table.insert(game.field, new_line)
+  orphan_check_timer.restart()
+end
+
+local float_timer = Timer(floating_figure_update_interval, function()
+  for x = 1, #game.field[1] do
+    if game.field[1][x] == Value.Floating then
+      -- float outside of the field
+      game.field[1][x] = Value.Free
+    end
+  end
+  for y = 2, #game.field do
+    for x = 1, #game.field[y] do
+      if game.field[y][x] == Value.Floating then
+        if game.field[y - 1][x] == Value.Free then
+          -- keep floating up
+          game.field[y - 1][x] = Value.Floating
+          game.field[y][x] = Value.Free
+        else
+          -- can't float! become solid
+          game.field[y][x] = Value.Solid
         end
       end
     end
   end
-  if time_since_floating_check_request then
-    time_since_floating_check_request = time_since_floating_check_request + dt
-    if time_since_floating_check_request > floating_check_interval then
-      time_since_floating_check_request = nil
-      mark_orphans()
+end)
+
+
+
+
+
+
+local add_timer
+add_timer = Timer(starting_adds_interval, function()
+  if starting_adds_left > 0 then
+    starting_adds_left = starting_adds_left - 1
+    if starting_adds_left == 0 then
+      game.generate_figure()
+      add_timer.interval = adds_interval
     end
   end
+  add_to_field()
+end)
+
+function game.start()
+  local field_size = { width = 10, height = 20 }
+  for y = 1, field_size.height do
+    game.field[y] = {}
+    for x = 1, field_size.width do
+      game.field[y][x] = Value.Free
+    end
+  end
+  game.current_figure = nil
+  starting_adds_left = math.floor(field_size.height / 3)
+
+  add_timer.start();
+  float_timer.start();
 
 end
 
-local function is_empty_line(y)
-  for x = 1, #game.field[y] do
-    if game.field[y][x] ~= Value.Free then
-      return false
+
+local function first_enabled_y(grid, x)
+  for y = 1, #grid do
+    if grid[y][x] == Value.Solid then
+      return y
     end
   end
-  return true
+  return nil
+end
+
+local function calc_selection_y()
+  local final_y = nil
+  for x = 1, #game.current_figure[1] do
+    local y1 = first_enabled_y(game.current_figure, x)
+    local y2 = first_enabled_y(game.field, x + x_selection - 1)
+    if y1 and y2 then
+      local y = y2 - y1 + 1
+      if final_y then
+        final_y = math.max(final_y, y)
+      else
+        final_y = y
+      end
+    end
+  end
+  if final_y and final_y < #game.field - #game.current_figure + 1 then
+    return final_y
+  else
+    return #game.field - #game.current_figure + 1
+  end
+end
+
+
+
+
+
+function love.update(dt)
+  for _, h in ipairs(update_handlers) do
+    h(dt)
+  end
 end
 
 local function round(x) return math.floor(x + 0.5) end
 
 local function choose_figure()
-  local d = 4
   for _ = 1, 500 do
-    game.current_figure = figures[d][math.random(1, #figures[d])]
+    game.current_figure = figures[math.random(1, #figures)]
     if is_removable_figure() then
       return
     end
@@ -321,7 +334,7 @@ function game.remove_figure()
       end
     end
   end
-  time_since_floating_check_request = 0
+  orphan_check_timer.restart()
   game.generate_figure()
   return true
 end
